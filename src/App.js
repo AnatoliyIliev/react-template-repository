170ce86d45fbbc94eeb49d72621e263652629d4f
Anatoliy_Iliev <vekok12@gmail.com>
2021-08-09 17:22:45 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+//  import 'modern-normalize/modern-normalize.css'
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
